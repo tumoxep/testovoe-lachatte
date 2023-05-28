f648fc6360becc668b7444a735a75998d2f87b4a
Тимофей Трофимов <t.trofimov@just-work.org>
2023-05-29 01:57:55 +0300

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/testovoe-lachatte/',
   plugins: [
     vue(),
     Components({
